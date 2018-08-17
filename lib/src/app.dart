0477707af9230df0089f09ab5a5fe171f0eec742
robin3317 <abdurrahmanrobin47@gmail.com>
2018-08-17 22:10:20 +0600

    set up UI

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Log Me In',
       home: Scaffold(
         body: LoginScreen(),
---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+          Container(margin: EdgeInsets.only(top: 20.0)),
+          submitButton(),
+        ],
+      ),
+    );
+  }
+
+  //helper classes
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'you@example.com',
+        labelText: 'EMail',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'Password',
+        labelText: 'Password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text('Login'),
+      color: Colors.blue,
+      onPressed: () {},
+    );
+  }
+}
