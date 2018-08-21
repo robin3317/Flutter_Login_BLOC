11419799b3e3bec26387902505958bff112605ef
robin3317 <abdurrahmanrobin47@gmail.com>
2018-08-21 20:11:39 +0600

    Provider in action

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
+import 'blocks/provider.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Log Me In',
-      home: Scaffold(
-        body: LoginScreen(),
+    return Provider(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Log Me In',
+        home: Scaffold(
+          body: LoginScreen(),
+        ),
       ),
     );
   }
