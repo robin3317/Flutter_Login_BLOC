8a16b41095a25327e3018e594f0d73149e365912
robin3317 <abdurrahmanrobin47@gmail.com>
2018-08-21 19:56:03 +0600

    Provider's constructor

--- lib/src/blocks/provider.dart
+++ lib/src/blocks/provider.dart
@@ -3,8 +3,12 @@ import 'block.dart';
 
 class Provider extends InheritedWidget {
   final block = Block();
+
+  Provider({Key key, Widget child}) : super(key: key, child: child);
+
   @override
   bool updateShouldNotify(_) => true;
+
   static Block of(BuildContext context) {
     return (context.inheritFromWidgetOfExactType(Provider) as Provider).block;
   }
